a08d3aad26d5774a60018021d79de5990cd9dbdf
snipe <snipe@snipe.net>
2015-07-20 17:38:36 -0700

    Website generation with Couscous

--- js/tipuesearch_set.js
+++ js/tipuesearch_set.js
@@ -27,9 +27,9 @@ var tipuesearch_replace = {'words': [
 // Weighting
 
 var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 200},
-     {'url': 'http://www.tipue.com/search', 'score': 100},
-     {'url': 'http://www.tipue.com/about', 'score': 100}
+     {'url': 'http://docs.snipeitapp.com/index.html', 'score': 100},
+     {'url': 'http://docs.snipeitapp.com/index.html', 'score': 100},
+     {'url': 'http://docs.snipeitapp.com/requirements.html', 'score': 100}
 ]};
 
 
